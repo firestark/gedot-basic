47aec3dbb688e7df7c0ab521ee682c59e76e76ba
Aron Wouters <lionar@mail.com>
2019-09-09 09:52:38 +0200

    added level bounds

--- scripts/Steve.gd
+++ scripts/Steve.gd
@@ -1,7 +1,44 @@
 extends KinematicBody
 
+var velocity = Vector3(0, 0, 0)
+const SPEED = 6
+const ROTATION_SPEED = 5.5
+
 func _ready():
 	pass
 
 func _physics_process(delta):
-	pass
+	
+	velocity = moveX(velocity);
+	velocity = moveZ(velocity);
+	
+	move_and_slide(velocity)
+	
+	
+func moveX(velocity : Vector3) -> Vector3:
+	if Input.is_action_pressed("ui_right") and Input.is_action_pressed("ui_left"):
+		velocity.x = 0
+	elif Input.is_action_pressed("ui_right"):
+		velocity.x = SPEED
+		$MeshInstance.rotate_z(deg2rad(-ROTATION_SPEED))
+	elif Input.is_action_pressed("ui_left"):
+		velocity.x = -SPEED
+		$MeshInstance.rotate_z(deg2rad(ROTATION_SPEED))
+	else:
+		velocity.x = lerp(velocity.x, 0, 0.1)
+		
+	return velocity
+	
+func moveZ(velocity : Vector3) -> Vector3:
+	if Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_down"):
+		velocity.z = 0
+	elif Input.is_action_pressed("ui_up"):
+		velocity.z = -SPEED
+		$MeshInstance.rotate_x(deg2rad(-ROTATION_SPEED))
+	elif Input.is_action_pressed("ui_down"):
+		velocity.z = SPEED
+		$MeshInstance.rotate_x(deg2rad(ROTATION_SPEED))
+	else:
+		velocity.z = lerp(velocity.z, 0, 0.1)
+		
+	return velocity
